dbae4f7974f02ab2f8bfe29d35c24431aefee049
Sadaka <pyrostack23@gmail.com>
2026-01-30 23:48:23 +0530

    Update contact page map coordinates to 6.317809, 80.426946

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -118,7 +118,7 @@ export default function ContactPage() {
                   </div>
                   <div className="w-full h-[400px] rounded-xl overflow-hidden">
                     <iframe
-                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.123456789!2d80.49106309265822!3d6.265632553666643!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwMTUnNTYuMyJOIDgwwrAyOScyNy44IkU!5e0!3m2!1sen!2slk!4v1234567890"
+                      src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3967.3456789!2d80.42469460000001!3d6.317809!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNsKwMTknMDQuMSJOIDgwwrAyNSczNy4wIkU!5e0!3m2!1sen!2slk!4v1234567890"
                       width="100%"
                       height="100%"
                       style={{ border: 0 }}
